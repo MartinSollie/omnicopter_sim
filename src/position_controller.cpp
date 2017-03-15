4d7edc9f486fa7f2ea53546f5cd88bc66b47d517
MartinSollie <martin.lysvand.sollie@gmail.com>
2017-03-15 13:01:04 +0100

    Add IMU callback to position controller

--- src/position_controller.cpp
+++ src/position_controller.cpp
@@ -2,11 +2,27 @@
 #include <omnicopter_sim/PosSp.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/Imu.h>
+#include <Eigen/Geometry>
 
 ros::Publisher force_pub;
+Eigen::Matrix3d R;
 
 void setpointCallback(const omnicopter_sim::PosSp& input){
+	if(input.type == omnicopter_sim::PosSp::SETPOINT_TYPE_FORCE){
+		// Transform input from ENU to BODY
+	}
 
+
+}
+
+void imuCallback(const sensor_msgs::Imu& input){
+	Eigen::Quaternion q;
+	q.x() = input.orientation.x;
+	q.y() = input.orientation.y;
+	q.z() = input.orientation.z;
+	q.w() = input.orientation.w;
+	R = q.toRotationMatrix();
 }
 
 int main(int argc, char **argv){
@@ -15,6 +31,7 @@ int main(int argc, char **argv){
 
 	ros::Subscriber pos_sp_sub = nh.subscribe("pos_sp",1,setpointCallback);
 	force_pub = nh.advertise<geometry_msgs::Vector3Stamped>("force_sp",0);
+	ros::Subscriber imu_sub = nh.subscribe("imu",1,imuCallback);
 
 	ros::spin();
 }
